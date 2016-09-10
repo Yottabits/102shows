ce680a0f3f9faac322a868cf765e3cd77015ee1f
Simon Leiner <simon@leiner.me>
2016-09-10 22:28:22 +0200

    implement fps measurement

--- lightshows/utilities.py
+++ lightshows/utilities.py
@@ -1,3 +1,7 @@
+import time
+import apa102
+
+
 def dim(undimmed: tuple, factor: float) -> tuple:
     """ multiply all components of :param undimmed with :param factor
     :return: resulting vector, as int
@@ -8,5 +12,39 @@ def dim(undimmed: tuple, factor: float) -> tuple:
         dimmed = dimmed + (i,)  # merge tuples
     return dimmed
 
+
 def fadeout(fadetime_sec: float):
-    pass # @todo
+    pass  # @todo
+
+
+class MeasureFPS:
+    """ measures the refresh rate available to the strip"""
+
+    def __init__(self, strip: apa102.APA102):
+        self.strip = strip
+        self.active_color = (255, 255, 255)
+        self.passed_color = (0, 100, 100)
+
+    def run(self) -> float:
+        """runs a test on the LED strip framerate
+        :return: a tuple with (framerate, time_elapsed, number_of_frames)
+        """
+        self.strip.clearStrip()
+        self.strip.clearStrip()  # just to be sure ;)
+
+        start_time = time.perf_counter()
+        for led in range(0, self.strip.numLEDs):
+            self.strip.setPixel(led, *self.active_color)
+            self.strip.show()
+            self.strip.setPixel(led, *self.passed_color)
+        stop_time = time.perf_counter()
+
+        time_elapsed = stop_time - start_time
+        number_of_frames = self.strip.numLEDs
+        framerate = number_of_frames / time_elapsed
+
+        time.sleep(1)
+        self.strip.clearStrip()
+        self.strip.clearStrip()
+
+        return (framerate, time_elapsed, number_of_frames)
--- testing.py
+++ testing.py
@@ -1,10 +1,12 @@
 import apa102
 import time
 import lightshows.spinthebottle
+import lightshows.utilities
 
 num_leds = 576
 strip = apa102.APA102(num_leds, max_spi_speed_hz=4000000)
-show = lightshows.spinthebottle.SpinTheBottle(strip)
-show.set_borders(0, 493)
+measurement = lightshows.utilities.MeasureFPS(strip)
 
-show.run()
+fps = measurement.run()
+
+print("Framerate: {}".format(fps))
