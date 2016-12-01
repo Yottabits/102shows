156831041aa76e4076c8a90a0516f798bb4e2713
Simon Leiner <simon@leiner.me>
2016-12-01 10:38:40 +0100

    PEP8

--- server/lightshows/dummy.py
+++ server/lightshows/dummy.py
@@ -9,8 +9,8 @@ import logging as log
 from drivers.fake_apa102 import APA102
 from DefaultConfig import Configuration
 
-
 minimal_number_of_leds = 1
+
 
 # end immediately after start
 def run(strip: APA102, conf: Configuration, parameters: dict):
