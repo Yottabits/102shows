8769dede96e776ee77b9b0421c645d7663a3874b
Simon Leiner <simon@leiner.me>
2016-12-08 18:23:59 +0100

    implemented glitch protection

--- server/mqtt/MQTTControl.py
+++ server/mqtt/MQTTControl.py
@@ -8,6 +8,8 @@ This script starts/stops the shows under lightshows/ according to the commands i
 import json
 import logging as log
 from multiprocessing import Process
+import threading
+import time
 
 import paho.mqtt.client
 import paho.mqtt.publish
@@ -23,7 +25,13 @@ class MQTTControl:
         # global handles
         self.conf = config  # the user config
         self.show_process = Process()  # for the process in which the lightshows run in
-        self.strip = None  # for the APA102 LED strip
+        self.strip = None  # for the LED strip
+
+        # initialize Anti Glitch Thread
+        self.anti_glitch_thread = threading.Thread(target=self.run_anti_glitch, daemon=True)
+        self.anti_glitch_delay_sec = 0.01
+        self.anti_glitch = True
+        self.anti_glitch_thread.start()
 
     # send to the MQTT notification channel: Node-RED will display a toast notification
     def notify_user(self, message, qos=0):
@@ -82,6 +90,20 @@ class MQTTControl:
         else:
             log.debug("MQTTControl ignored {show}:{command}".format(show=show_name, command=command))
 
+    def run_anti_glitch(self):
+        """
+        This function should be called periodically to write the buffer to the strip.
+        That is useful for cases where plugging or unplugging another electric device
+        in the room induces a glitch in the LED strip. This glitch will be visible
+        until the next call of strip.show(). Because of that it is useful to write the
+        buffer to the strip every now and then.
+        """
+        while self.anti_glitch is True:
+            if not self.show_process.is_alive() and self.anti_glitch:  # execute only if no lightshow is running
+                log.debug("Invoking periodic show() command")
+                self.strip.show()
+                time.sleep(self.anti_glitch_delay_sec)
+
     def start_show(self, show_name: str, parameters: dict):
         # search for show module
         if show_name not in self.conf.shows:
@@ -139,6 +161,8 @@ class MQTTControl:
             client.username_pw_set(self.conf.MQTT.username, self.conf.MQTT.password)
         client.connect(self.conf.MQTT.Broker.host, self.conf.MQTT.Broker.port, self.conf.MQTT.Broker.keepalive)
         log.info("{name} is ready".format(name=self.conf.sys_name))
+
+
 
         client.loop_forever()
         log.critical("MQTTControl.py has exited")
