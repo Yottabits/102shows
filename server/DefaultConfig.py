6d3ef946a45adc2815c4d45328fac33293009f37
Simon Leiner <simon@leiner.me>
2016-12-11 17:38:29 +0100

    entries in configuration

--- server/DefaultConfig.py
+++ server/DefaultConfig.py
@@ -12,6 +12,7 @@ class Configuration:
     log_level = log.INFO  # standard log level
 
     shows = {'clear': clear.Clear,  # A list of available shows
+             'christmas': christmas.Christmas,
              'rainbow': rainbow.Rainbow,
              'rgbtest': rgbtest.RGBTest,
              'spinthebottle': spinthebottle.SpinTheBottle,
--- server/lightshows/__init__.py
+++ server/lightshows/__init__.py
@@ -6,6 +6,7 @@ That's what it's all about.
 """
 
 __all__ = ['clear',
+           'christmas',
            'rainbow',
            'rgbtest',
            'solidcolor',
