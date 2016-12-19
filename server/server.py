e86393da55eabbb2d3f0b928d72c308fc61d0a2c
Simon Leiner <simon@leiner.me>
2016-12-19 22:23:41 +0100

    added greeting and version number

--- server/server.py
+++ server/server.py
@@ -26,12 +26,30 @@ import coloredlogs
 from helpers.configparser import get_configuration
 from mqttcontrol import MQTTControl
 
+__version__ = '0.2pre-dev'
+
+logo = """
+   _______ ___        __
+  <  / __ \__ \ _____/ /_  ____ _      _______
+  / / / / /_/ // ___/ __ \/ __ \ | /| / / ___/
+ / / /_/ / __/(__  ) / / / /_/ / |/ |/ (__  )
+/_/\____/____/____/_/ /_/\____/|__/|__/____/  v{}"""
+
 # configuration
 user_config = get_configuration()
 
 # logger
 logger = logging.getLogger('102shows.server')
 coloredlogs.install(level=user_config.log_level)
+
+# friendly greetings
+print(logo.format(__version__))
+print()
+print("This is free software. You are welcome to redistribute")
+print("it under the conditions of the GNU Public License v2.")
+print("For details, see https://www.gnu.org/licenses/gpl-2.0.html")
+print()
+print()
 
 # start the server!
 server = MQTTControl(user_config)  # initialize the server...
