fcefb23be2cced66f422e4fedb14ccef9a32046e
Simon Leiner <simon@leiner.me>
2017-01-14 15:03:44 +0100

    Revert "test sphix-autodoc-annotation instead"
    This reverts commit f8383a6f990aebb0bb7344d11b3127cb25283c9d.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ import sphinx_rtd_theme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx_autodoc_annotation',
+              'sphinx_autodoc_typehints',
               'sphinx.ext.intersphinx',
               'sphinx.ext.todo',
               'sphinx.ext.coverage',
