7ac4573599e2b2bd74492c76061de21999b1116a
Simon Leiner <simon@leiner.me>
2017-01-14 16:39:11 +0100

    side-wide documentation

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,7 @@ todo_include_todos = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_style = 'css/custom_theme.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
